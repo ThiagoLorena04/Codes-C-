99b8943adfb6af28c63373cb482e6b9cbad0e870
Thiago Vitor Lorena <thiago.vitor.2067@gmail.com>
2025-02-12 15:01:47 +0000

    Colocando projetopessoal de teste

---
+++ ProjetoPessoal01 - Calculadora/projetoPessoal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    printf("oiii, escreva um numero aleatorio foi: ");
+    int oi = 0;
+    scanf("%d", &oi);
+    
+
+    printf("\nSeu numero escolhido foi de %d\n", oi);
+}

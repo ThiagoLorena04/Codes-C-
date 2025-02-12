d44a699a5a7baa7f16ca76407f381281f46c58a0
Thiago Vitor Lorena <thiago.vitor.2067@gmail.com>
2025-02-12 15:27:16 +0000

    Primeiro commit do projeto

---
+++ ProjetoPessoal01 - Calculadora/calculadora.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Exibe uma mensagem simples
+    printf("Hello, World!\n");
+
+    // Teste com variáveis
+    int a = 5, b = 3;
+    int soma = a + b;
+
+    // Exibe o resultado da soma
+    printf("A soma de %d e %d é: %d\n", a, b, soma);
+
+    return 0;
+}

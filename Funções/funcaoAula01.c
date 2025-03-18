acdc7dbb71d7fb106a22d96b90208b58268c595d
Thiago Vitor Lorena <thiago.vitor.2067@gmail.com>
2025-03-18 15:30:27 +0000

    adicionando novoc exer

---
+++ Funções/funcaoAula01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma(int a, int b){
+    int c;
+    c = a + b;
+    return c;
+}
+
+
+int main (){
+    int r = soma (10, 10);
+    printf("\n\n\n\n%d", r);
+}
---
+++ exer-lerArea-funcao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int ler-dados(){
+    int aux;
+
+    printf("Digite um valor")
+}
+
+int main(){
+
+    int base, altura;
+
+    ler-dados()
+}
